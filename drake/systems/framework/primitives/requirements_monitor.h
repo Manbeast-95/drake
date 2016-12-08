c4e3b4ecad7371f3aa7fc14d840c8a55a1cc57a9
Russ Tedrake <russt@mit.edu>
2016-12-08 08:47:59 -0500

    WIP

--- drake/systems/framework/primitives/requirements_monitor.h
+++ drake/systems/framework/primitives/requirements_monitor.h
@@ -3,6 +3,8 @@
 #include "drake/solvers/decision_variable.h"
 #include "drake/solvers/constraint.h"
 
+namespace drake {
+namespace systems {
 
 enum {
   kThrowException = 1,
@@ -17,21 +19,36 @@ public:
   }
 
   // Get the decision variable associated with simulation clock time.
-  DecisionVariable time();
+  solvers::DecisionVariableVectorX time();
   
   // Create a new input port for the system.  Returns a vector of decision variables.
-  DecisionVariableVector AddVectorInput(size);
+  DecisionVariableVectorX AddVectorInput(size);
   
   // Add a new requirement to the system.
   void AddConstraint(std::shared_ptr<Constraint> con,
                      const VariableListRef& vars, RequirementFailureAction action);
 
   std::unique_ptr<RequirementsMonitor> Build();
+  
+  DoPublish(const Context<double>& context) {
+    time() = context.get_time();
+    
+    // Reads all of the inputs.
+    for (const auto& input : input_ports_) {
+      input.WriteThrough(this->EvalVectorInput(context,input_.get())->CopyToVector(),variables_)
+    }
+    
+    // Evaluates all of the constraints.
+  }
 
 private:
   DecisionVariableVectorX variables_;
-  std::vector<Binding<Constraint>> constraints_;  // Note: currently Binding is a private class INSIDE MathematicalProgram.  Can we extract it?
+  std::vector<DecisionVariableConstraintBinding<Constraint>> constraints_;
+  std::vector<DecisionVariableBinding<SystemPortDescriptor<double>> input_ports;
   
   // TODO: add a list of system ports (and their bindings to decision variables)
 }
+
+}  // namespace systems
+}  // namespace drake
 
